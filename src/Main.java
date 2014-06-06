fe3670885ab0aa8529d1b89055ae4622147181cf
Chanwoo Jeong <enghqii@gmail.com>
2014-06-06 18:03:58 +0900

    Microba DatePicker lib added

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,10 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-
 public class Main {
 
 	 public static void main(String[] args) throws ClassNotFoundException
-	  {
+	  {		 
 	    // load the sqlite-JDBC driver using the current class loader
 	    Class.forName("org.sqlite.JDBC");
 
