cc3f078c90e3f993f52f7846e45cc55c6f757a2c
Chanwoo Jeong <enghqii@gmail.com>
2014-06-06 21:29:11 +0900

    ui design

--- src/UsagePanel.java
+++ src/UsagePanel.java
@@ -1,10 +1,16 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.table.DefaultTableColumnModel;
-import javax.swing.table.TableColumn;
-import javax.swing.table.TableColumnModel;
+
+import com.michaelbaranov.microba.calendar.DatePicker;
 
 public class UsagePanel extends JPanel {
 
@@ -15,11 +21,22 @@ public class UsagePanel extends JPanel {
 	}
 
 	private void initUI() {
+		
 		setLayout(null);
 
+		// 타이틀 라벨
 		JLabel titleLabel = new JLabel("실시간 사용 현황");
 		titleLabel.setBounds(450, 20, 100, 30);
 		this.add(titleLabel);
+		
+		// 오늘의 라벨
+		DateFormat sdFormat = new SimpleDateFormat("yyyy년 MM월 dd일");
+		Date nowDate = new Date();
+		String tempDate = sdFormat.format(nowDate);
+		
+		JLabel todayLabel = new JLabel(tempDate);
+		todayLabel.setBounds(500 + 300 - 50, 20, 100, 30);
+		this.add(todayLabel);
 
 		/*
 		 * JTable roomList = new JTable(); TableColumnModel columnModel = new
@@ -33,9 +50,10 @@ public class UsagePanel extends JPanel {
 		 * 400);
 		 */
 
+		// 사용현황 테이블
 		Object rowData[][] = {
-				{ "Row1-Column1", "Row1-Column2", "Row1-Column3" },
-				{ "Row2-Column1", "Row2-Column2", "Row2-Column3" } };
+				{ "Row1-Column1", "Row1-Column2"},
+				{ "Row2-Column1", "Row2-Column2"} };
 		Object columnNames[] = { "Column One", "Column Two" };
 		JTable table = new JTable(rowData, columnNames);
 
@@ -44,7 +62,28 @@ public class UsagePanel extends JPanel {
 		
 		scrPane.setBounds(30, 70, 600, 400);
 		this.add(scrPane);
-
+		
+		// 데이트 피커
+		DatePicker datePicker = new DatePicker(new Date());
+		datePicker.setBounds(30, 500, 300, 25);
+		this.add(datePicker);
+		
+		// 콤보박스
+		JComboBox<String> comboBox = new JComboBox<String>();
+		String[] a = {"room1", "room2"};
+		comboBox.setModel(new DefaultComboBoxModel<String>(a));
+		comboBox.setBounds(370, 500, 100, 25);
+		this.add(comboBox);
+		
+		// 상세 정보 조회
+		JButton detail = new JButton("상세 정보 조회");
+		detail.setBounds(500, 500, 100, 25);
+		this.add(detail);
+		
+		// 사용자 예약
+		JButton reserv = new JButton("사용자 예약");
+		reserv.setBounds(650, 500, 100, 25);
+		this.add(reserv);
 	}
 
 }
